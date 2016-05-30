c6212d1bbfbe881bef743d144474e9444aee55b7
p1411805 <Epulapp@EPULAPP59.univ-lyon1.fr>
2016-05-30 09:59:54 +0200

    main

--- FormaSearch/src/formasearch/FormaSearch.java
+++ FormaSearch/src/formasearch/FormaSearch.java
@@ -5,7 +5,9 @@
  */
 package formasearch;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -17,19 +19,31 @@ public class FormaSearch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String fichierLieuFormation ="C:\\Users\\Epulapp\\Documents\\ProjetOptiDiscrete\\FormaSearch\\Sources\\LieuxPossibles.txt";
-        List<LieuFormation> formations = Utilitaire.getLieuFormationFichier(fichierLieuFormation);
-        /*for(LieuFormation f:formations){
-            System.out.println(f.toString());
-        }*/
         
+        //Récupération de la liste des agences à partir du fichier
         String fichierAgence ="C:\\Users\\Epulapp\\Documents\\ProjetOptiDiscrete\\FormaSearch\\Sources\\ListeAgences_100.txt";
         List<Agence> agences = Utilitaire.getAgenceFichier(fichierAgence);
-        LieuFormation formation=null;
-        for(Agence a:agences){
-            formation = a.getLieuFormationPlusProche(formations);
-        }
+        int nbAgences = (int)agences.size()*20/100;
+               
+        //Récupération de la liste des formations à partir du fichier
+        String fichierLieuFormation ="C:\\Users\\Epulapp\\Documents\\ProjetOptiDiscrete\\FormaSearch\\Sources\\LieuxPossibles.txt";
+        List<LieuFormation> formations = Utilitaire.getLieuFormationFichier(fichierLieuFormation);
         
+        //On mélange la liste des formations
+        long seed = System.nanoTime();
+        Collections.shuffle(formations, new Random(seed));
+        
+        //On prend x lieux de formations eq à 20% du nombre d'agences
+        List<LieuFormation> formationsTest = formations.subList(0, nbAgences);
+        for(LieuFormation f:formationsTest){
+            System.out.println(f.toString());
+        }
+            
+        for(Agence a:agences){
+            LieuFormation formation = a.getLieuFormationPlusProche(formations);
+        }
+
+        /* Test méthode  getDistance
         Ville v1 = new Ville();
         v1.setLatitude(46.25);
         v1.setLongitude(6.01667);
@@ -39,9 +53,7 @@ public class FormaSearch {
         v2.setLongitude(5.2);
         
         double distance = v1.getDistance(v2);
-        System.out.println(distance);
-        
-        
+        System.out.println(distance);*/  
         
     }
     
